5b6b53451ff197683e38cf2520866ff2c7eaef1d
kumaresh <kumareshkumareee75@gmail.com>
2022-09-22 15:41:41 +0530

    map push

---
+++ src/map/mapjava.java
@@ -0,0 +1,26 @@
+package map;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class mapjava {
+	
+	@SuppressWarnings("unchecked")
+	public static void main(String[] args) {
+		@SuppressWarnings({ "rawtypes", "unused" })
+		Map mi=new HashMap();
+		mi.put(10, "java");
+		mi.put(10,"selenium");
+		System.out.println(mi);
+
+		
+		
+	
+		
+		
+		
+		
+		
+	}
+
+}
